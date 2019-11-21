b3817f0491f65f4bec338af1bc9e10cbee25a3fa
authorabby <abllee@ucdavis.edu>
2019-11-21 13:17:15 -0800

    Started pseudocode

---
+++ maxTweeter.c
@@ -0,0 +1,30 @@
+
+/*
+Homework 4
+Abigail Lee - 913790818 abllee@ucdavis.edu
+Luc Nglankong -
+
+*/
+
+#include <stdio.h>
+
+// define :
+// max line 1024 characters
+// max length = 20,000 lines
+
+int main() {
+// goal - find and print top 10 tweeters ordered in decreasing order
+
+  // accept csv file
+    // check if invalid file type
+    // check for valid "header" - could be anywhere in file
+    // check for valid "name" - could be anywhere in file
+    // items may or may not be in quotes
+
+
+  // loop/stream? through tweets and track number of tweets for every tweeter
+  // sort tweeters by count
+  // print top 10
+
+
+}
