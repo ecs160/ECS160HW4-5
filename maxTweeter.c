c3cce38d3f7d64c7aa9d0825acc2fe21fd553a7c
authorabby <abllee@ucdavis.edu>
2019-11-22 15:03:57 -0800

    Attempt 1 at csv reading

--- maxTweeter.c
+++ maxTweeter.c
@@ -15,20 +15,43 @@ Luc Nglankong -
 // max line 1024 characters
 // max length = 20,000 lines
 
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ";");
+            tok && *tok;
+            tok = strtok(NULL, ";\n"))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
+
 int main() {
 // goal - find and print top 10 tweeters ordered in decreasing order
 
   // accept csv file
   // https://stackoverflow.com/questions/12911299/read-csv-file-in-c
 
-  // FILE* stream = fopen("tweetFile", "r");
-  // char line[1024];
-  // while (fgets(line, 1024, stream)) {
-  //
-  // }
+  #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+
+    FILE* stream = fopen("ECS160TestCSV - simplecsv.csv", "r");
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char* tmp = strdup(line);
+        printf("Field 3 would be %s\n", getfield(tmp, 3));
+        // NOTE strtok clobbers tmp
+        free(tmp);
+    }
   printf("Hello world!");
-  return 0; 
+  return 0;
     // check if invalid file type
     // check for valid "header" - could be anywhere in file
     // check for valid "name" - could be anywhere in file
