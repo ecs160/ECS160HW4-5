a5a66e6d37cb0d77c885f2079c139bd0196f3b2d
lnglankong <lnglankong@ucdavis.edu>
2019-11-30 20:47:54 -0800

    added consistent quote for column check, added check for correct # of columns

--- maxTweeter.c
+++ maxTweeter.c
@@ -13,7 +13,7 @@ Luc Nglankong -
     // check if length of row exceeds 1024 characters
     // check for valid "header" - could be anywhere in file
     // check for valid "name" - could be anywhere in file
-    // items may or may not be in quotes
+    // Done - items may or may not be in quotes
     // else - Invalid Input Format
 
 
@@ -22,6 +22,8 @@ Luc Nglankong -
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+
 
 #define MAX_LINE 1024
 #define MAX_LENGTH 20000
@@ -38,7 +40,11 @@ int findNameCol(char*);
 struct tweeter *getTweeter(struct tweeter*, char*, int);
 void printTweeters(struct tweeter*, int);
 struct tweeter * findTopTenTweeters(struct tweeter*, int);
-int validateName(int indexName, char* row);
+int validateHeader(int indexName, char* row, bool isName);
+bool quoteScanner(char* phrase);
+int getNumColumns(char* row);
+bool getQuoteIndex(char* header, int columnIndex);
+bool checkColumnQuotes(char* row, bool quoteIndex[], int numColumns);
 
 int main() {
 
@@ -47,13 +53,13 @@ int main() {
   // struct tweeter* tweeter_ptr = tweeters; 
   // accept csv file
   // https://stackoverflow.com/questions/12911299/read-csv-file-in-c
-    const char* filePath = "./Test CSV/csv3.csv";
+    const char* filePath = "./Test CSV/csv2.csv";
     FILE* csvFileStream = fopen(filePath, "r");
 
     // check if invalid file type
     if (!csvFileStream) {
       printf("Invalid Input Format\n");
-      return 0;
+      return -1;
     }
 
     // FIND NAME COLUMN INDEX 
@@ -61,18 +67,35 @@ int main() {
     int lineCount = 0;
     char *tmp;
     tmp = fgets(row, 1024,csvFileStream);
+
     char* validationRow = strdup(row); //save copy of row for validation
+    char* numColumnsRow = strdup(row); //save copy of row for finding # of columns
+    char* quoteIndexRow = strdup(row); //save copy of row for getting column quote index
+
     int indexName = findNameCol(tmp);
-    int validateNameResult = validateName(indexName-1, validationRow); //validate name header
+    int numColumns = getNumColumns(validationRow); //get the number of columns
+
+    int validateNameResult = validateHeader(indexName, validationRow, true); //validate name header
+    
+    // get which columns should be wrapped in quotes
+    // quoteIndex[i] == true - should be wrapped in quotes
+    // quoteIndex[i] == false - should not be wrapped in quotes
+    bool quoteIndex[numColumns];
+    for(int i=1; i<=numColumns; i++){
+      quoteIndex[i] = getQuoteIndex(quoteIndexRow, i);
+    }
+
+    free(validationRow);
+    free(numColumnsRow);
+    free(quoteIndexRow);
     if(validateNameResult < 1){
-      printf("Invalid header format\n"); // name header is invalid
       return -1;
     }
    
     if (indexName == -1){
       printf("Invalid Input Format\n"); // name column not found
+      return -1;
     } 
-
     
     //tweeter *newTweeter = malloc(sizeof(tweeter));
    
@@ -83,6 +106,18 @@ int main() {
     {
         char* tmp = strdup(row);
         str = getfield(tmp, indexName);
+
+        //validate that current row has the correct # of columns
+        if(getNumColumns(tmp) != numColumns){
+          printf("Invalid csv: a row contains an incorrect # of columns.\n");
+          return -1;
+        }
+
+        //validate that the columns that should be wrapped in quotes are
+        //correctly wrapped in quotes
+        if(!checkColumnQuotes(tmp, quoteIndex, numColumns)){
+          return -1;
+        }
 
         //search for existing tweeter
         struct tweeter * currentTweeter = getTweeter(tweeters, str, numTweetersTotal);
@@ -112,34 +147,155 @@ int main() {
 
 }
 
-int validateName(int indexName, char* row){
-  int cellIndex = 0;
-  int nameCharIndex = 0;
+bool checkColumnQuotes(char* row, bool *quoteIndex, int numColumns){
+  
+  //iterate through each cell in row
+  for(int i=1; i<=numColumns; i++){
 
-  for(int i=0; i<strlen(row) && nameCharIndex < 1; i++){
-    // increment cell index when a comma is encountered
-    if(row[i] == ','){
-      cellIndex++;
-    }
-
-    // when the current cell matches the 
-    // given "name" header cell, save the char index
-    if(cellIndex == indexName){
-      nameCharIndex = i;
-    }
-  }
-
-  //validate for whitespace
-  for(int i=nameCharIndex+1; (row[i] != '\0') && (row[i] != '\n') && 
-    (row[i] != ',') && (row[i] != '\r'); i++){
-
-      // check if there is a whitespace in name header
-      if(isspace(row[i])){
-        // return invalid
-        return 0;
+    //goto current cell
+    int currCellIndex = 1;
+    int currCharIndex = 0;
+    for(; currCellIndex != i;){
+      if(row[currCharIndex] == ','){
+        currCellIndex++;
       }
+      currCharIndex++;
+    }
+
+    //get cell size
+    size_t cellSize = 0;
+    for(int j=currCharIndex; (row[j] != '\0') && (row[j] != '\n') 
+        && (row[j] != ',') && (row[j] != '\r') && (j<strlen(row)); j++){
+      cellSize++;
+    }
+
+    //get substring cell from row
+    char* substr = malloc(cellSize+1);
+    strncpy(substr, &row[currCharIndex], cellSize);
+    substr[cellSize] = '\0';
+
+    //check for consistent quotes on cell entry
+    if(!quoteScanner(substr)){
+      printf("Invalid csv: a row does not contain consistent quotes.\n");
+      return false;
+    }
+
+    //if the cell's header has quotes but the cell doesn't
+    if((substr[0] == '"' && !quoteIndex[i])){
+      printf("Invalid csv: a column that must not contain quotes contains an entry with quotes.\n");
+      return false;
+    }
+
+    //if the cell's header doesn't have quotes but the cell does, return invalid
+    if(substr[0] != '"' && quoteIndex[i]){
+      printf("Invalid csv: a column that must contain quotes contains an entry without quotes.\n");
+      return false;
+    }
   }
-  // return valid
+
+  return true;
+}
+
+
+// return whether or not a column should be wrapped in quotes
+bool getQuoteIndex(char* header, int columnIndex){
+  
+  // navgiate to column index
+  int currCellIndex = 1;
+  int currCharIndex = 0;
+  for(; currCellIndex != columnIndex;){
+    if(header[currCharIndex] == ','){
+      currCellIndex++;
+    }
+    currCharIndex++;
+  }
+
+  // if the first character of the column is a quote, then 
+  // we know that this entire column should be wrapped in quotes.
+  // We don't need to check if the last character is a quote because
+  // we already validated that the header is properly wrapped in 
+  // quoteScanner()
+  // if a "," is found, then the column header is empty and entries shouldn't
+  // be wrapped in quotes.
+  if(header[currCharIndex] == '"' && header[currCharIndex] != ','){
+    return true;
+  }
+
+  // if the first character was not a quote or the column is
+  // empty, then this column should not be wrapped in quotes
+  return false;
+}
+
+// given the csv header, find the number of columns
+int getNumColumns(char* header){
+  int numColumns=0;
+  for(int i=0; i<strlen(header); i++){
+    if(header[i] == ','){
+      numColumns++;
+    }
+  }
+  numColumns++;
+
+  return numColumns;
+}
+
+bool quoteScanner(char*phrase) {
+  bool startQuote = false;
+  bool endQuote = false;
+
+  for (int i = 0; i < strlen(phrase); i++ ){
+    if (phrase[i] == '"') {
+        //  printf("\nfound start quote\n");
+        startQuote = true;
+    }
+    i++;
+    if ((startQuote) && (phrase[i] =='"')){
+        // printf("\nfound end quote\n");
+      endQuote = true;
+    }
+  }
+  if((startQuote && endQuote) || (!startQuote && !endQuote)){
+     return true;
+  }
+
+  return false;
+}
+
+// validate header
+int validateHeader(int indexName, char* row, bool isName){
+
+  //go to index of cell in row
+  int currCellIndex = 1;
+  int currCharIndex = 0;
+  for(; currCellIndex != indexName;){
+    if(row[currCharIndex] == ','){
+      currCellIndex++;
+    }
+    currCharIndex++;
+  }
+
+  //get size of cell
+  size_t cellSize = 0;
+  for(int i=currCharIndex; (row[i] != '\0') && (row[i] != '\n') 
+      && (row[i] != ',') && (row[i] != '\r') && (i<strlen(row)); i++){
+
+    // invalid if there is a space
+    if(isspace(row[i]) && isName){
+      printf("Invalid header: name column must not contain whitespaces.\n");
+      return -1;
+    }
+    cellSize++;
+  }
+
+  //get substring cell from row
+  char* substr = malloc(cellSize+1);
+  strncpy(substr, &row[currCharIndex], cellSize);
+  substr[cellSize] = '\0';
+
+  if(!quoteScanner(substr)){
+    printf("Invalid header: inconsistent quotes.\n");
+    return -1;
+  }
   return 1;
 }
 
